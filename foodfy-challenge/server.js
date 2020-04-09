df75b28a0936fda4daff9ab13e36399829f8b075
PivaDesk <piiiiva@gmail.com>
2020-04-09 16:15:21 -0300

    Refactoring Foodfy
    Server creation and configuration with Express; Routes created for test; Install and Config Nunjucks; Rename html to njk

---
+++ foodfy-challenge/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+server.set('view engine', "html")
+
+nunjucks.configure('views', {
+    express: server,
+    noCache: true,
+    autoescape: false,
+})
+
+
+server.get('/', function(req, res){
+    return res.render('index')
+})
+
+server.get('/about', function(req, res){
+    return res.render('about')
+})
+
+server.get('/recipes', function(req, res){
+    return res.render('recipes')
+})
+
+server.listen(5000, function(){
+    console.log('Server FoodFy is Running')
+})
