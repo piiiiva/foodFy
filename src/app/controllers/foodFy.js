fdd0ff12ff179e1deb6a83cfa591f7c0b5ebd033
PivaDesk <piiiiva@gmail.com>
2020-06-24 23:23:11 -0300

    Challenge 5: Persisting data - Using sql database in foodfy Controller, index() method

--- src/app/controllers/foodFy.js
+++ src/app/controllers/foodFy.js
@@ -1,20 +1,21 @@
-
+const Recipe = require('../models/Recipe')
 const data = require('../../../data.json')
 
-exports.index = (req, res) => {
-    return res.render('foodFy/index', { recipes: data.recipes })
-}
-
-exports.about = function(req, res){
-    return res.render('foodFy/about')
-}
-
-exports.recipes = function(req, res){
-    return res.render('foodFy/recipes', { recipes: data.recipes })
-}
-
-exports.show = function(req, res){
-    const recipeIndex = req.params.index
-
-    return res.render('foodFy/recipe', { recipe: data.recipes[recipeIndex] })
+module.exports = {
+    index(req, res) {
+        Recipe.all(function(recipes) {
+            return res.render('foodfy/index', { recipes })
+        })
+    },
+    about(req, res) {
+        return res.render('foodFy/about')
+    },
+    recipes(req, res) {
+        return res.render('foodFy/recipes', { recipes: data.recipes })
+    },
+    show(req, res) {
+        const recipeIndex = req.params.index
+    
+        return res.render('foodFy/recipe', { recipe: data.recipes[recipeIndex] })
+    },
 }
